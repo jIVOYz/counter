faa1a2baba912e6be82dbed93a60ba977c9a0f00
jIVOYz <80265811+jIVOYz@users.noreply.github.com>
2021-08-19 18:44:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let result = document.getElementById('result');
+let integer = 0;
+
+let add = document.getElementById('plus');
+let remove = document.getElementById('minus');
+let reset = document.getElementById('reset');
+
+
+    add.addEventListener("click", function () {
+        integer += 1;
+        result.innerHTML = integer;
+    });
+
+
+    remove.addEventListener("click", function () {
+        integer -= 1;
+        result.innerHTML = integer;
+    });
+
+    reset.addEventListener("click", function () {
+        integer = 0;
+        result.innerHTML = integer;
+    });
+
